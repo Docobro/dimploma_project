66136ccd255e490e1a98f6e18c0408c6a584166c
fr11nik <fruinik@gmail.com>
2024-05-05 20:41:54 +0300

    refactor:change migration to Clickhouse scheme

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,58 +1,121 @@
-CREATE TABLE cryptowallet.currencies (
-  id UUID PRIMARY KEY,
-  name String NOT NULL,
-  code String NOT NULL,
-  max_supply Int64 DEFAULT 9999,
-  description String
-) ENGINE = MergeTree()
-ORDER BY id;
+-- cryptowallet.currencies определение
 
-CREATE TABLE cryptowallet.trade_volume_1h (
-  id UUID PRIMARY KEY,
-  crypto_id Int64 NOT NULL,
-  trade_volume Float32 NOT NULL,
-  time_diff DateTime
-) ENGINE = MergeTree()
-ORDER BY id;
+CREATE TABLE cryptowallet.currencies
+(
 
-CREATE TABLE cryptowallet.indices (
-  id UUID PRIMARY KEY,
-  crypto_id Int64 NOT NULL,
-  price_index Int32 NOT NULL,
-  volume_index Float32 NOT NULL,
-  created_at DateTime DEFAULT now()
-) ENGINE = MergeTree()
-ORDER BY id;
+    `id` UUID,
 
-CREATE TABLE cryptowallet.prices (
-  id UUID PRIMARY KEY,
-  crypto_id Int64 NOT NULL,
-  value Decimal(18, 2) NOT NULL,
-  market_cap Int32 NOT NULL,
-  time_diff DateTime,
-  created_at DateTime DEFAULT now()
-) ENGINE = MergeTree()
-ORDER BY id;
+    `name` String,
 
-CREATE TABLE cryptowallet.transaction_per_day (
-  id UUID PRIMARY KEY,
-  crypto_id Int64 NOT NULL,
-  trans_value Int64 NOT NULL,
-  created_at DateTime DEFAULT now()
-) ENGINE = MergeTree()
-ORDER BY id;
+    `code` String,
 
-CREATE TABLE cryptowallet.supplies (
-  id UUID PRIMARY KEY,
-  crypto_id Int64 NOT NULL,
-  total_supply Int64,
-  created_at DateTime DEFAULT now()
-) ENGINE = MergeTree()
-ORDER BY id;
+    `max_supply` Int64 DEFAULT 9999,
 
-ALTER TABLE cryptowallet.trade_volume_1h ADD COLUMN IF NOT EXISTS crypto_id Int64;
-ALTER TABLE cryptowallet.indices ADD COLUMN IF NOT EXISTS crypto_id Int64;
-ALTER TABLE cryptowallet.prices ADD COLUMN IF NOT EXISTS crypto_id Int64;
-ALTER TABLE cryptowallet.transaction_per_day ADD COLUMN IF NOT EXISTS crypto_id Int64;
-ALTER TABLE cryptowallet.supplies ADD COLUMN IF NOT EXISTS crypto_id Int64;
+    `description` String
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
 
+
+-- cryptowallet.indices определение
+
+CREATE TABLE cryptowallet.indices
+(
+
+    `id` UUID,
+
+    `volume_index` Float32,
+
+    `created_at` DateTime DEFAULT now(),
+
+    `crypto_id` UUID,
+
+    `price_index` Float64
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
+
+
+-- cryptowallet.prices определение
+
+CREATE TABLE cryptowallet.prices
+(
+
+    `id` UUID,
+
+    `value` Decimal(18,
+ 2),
+
+    `time_diff` DateTime,
+
+    `created_at` DateTime DEFAULT now(),
+
+    `crypto_id` UUID,
+
+    `market_cap` Float64
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
+
+
+-- cryptowallet.supplies определение
+
+CREATE TABLE cryptowallet.supplies
+(
+
+    `id` UUID,
+
+    `crypto_id` Int64,
+
+    `total_supply` Int64,
+
+    `created_at` DateTime DEFAULT now()
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
+
+
+-- cryptowallet.trade_volume_1h определение
+
+CREATE TABLE cryptowallet.trade_volume_1h
+(
+
+    `id` UUID,
+
+    `crypto_id` Int64,
+
+    `trade_volume` Float32,
+
+    `time_diff` DateTime
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
+
+
+-- cryptowallet.transaction_per_day определение
+
+CREATE TABLE cryptowallet.transaction_per_day
+(
+
+    `id` UUID,
+
+    `trans_value` Int64,
+
+    `created_at` DateTime DEFAULT now(),
+
+    `crypto_id` UUID
+)
+ENGINE = MergeTree
+PRIMARY KEY tuple(id)
+ORDER BY id
+SETTINGS index_granularity = 8192;
